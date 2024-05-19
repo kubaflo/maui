f2a4cd2b111fa404313669ed85f6e48f54af9e51
Jakub Florkowski <kubaflo123@gmail.com>
2024-05-19 17:15:12 +0200

    Added a UI test

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue15524.cs
@@ -0,0 +1,32 @@
+﻿#if ANDROID
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue15524 : _IssuesUITest
+{
+	public override string Issue => "Text entry border disappear when changing to/from dark mode";
+
+	public Issue15524(TestDevice device) : base(device) { }
+
+	[Test]
+	public async Task TextEntryBorderShouldNotDisappearOnThemeChange()
+	{
+		try
+		{
+			App.WaitForElement("entry");
+			App.SetDarkTheme();
+			await Task.Delay(5000);
+
+			//The test passes if the entry border is visible
+			VerifyScreenshot();
+		}
+		finally
+		{
+			App.SetLightTheme();
+		}
+	}
+}
+#endif
---
+++ src/Controls/tests/TestCases/Issues/Issue15524.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[Issue(IssueTracker.Github, 15524, "Text entry border disappear when changing to/from dark mode", PlatformAffected.Android)]
+	public partial class Issue15524 : ContentPage
+	{
+		public Issue15524()
+		{
+			InitializeComponent();
+		}
+	}
+}
