d37d8584b0c34603ab695e73b423900dcb4b1d57
Jakub Florkowski <kubaflo123@gmail.com>
2024-05-19 20:37:58 +0200

    Addd a UI Test 2

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue14793.cs
@@ -0,0 +1,24 @@
+﻿#if ANDROID
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues
+{
+	public class Issue14793 : _IssuesUITest
+	{
+		public override string Issue => "[Android] Toolbar's text style for default theme has too low contrast";
+
+		public Issue14793(TestDevice device) : base(device) { }
+
+		[Test]
+		public void PageTitleShouldBeWhite()
+		{
+			App.WaitForElement("label");
+
+			// The test passes if the page title is white
+			VerifyScreenshot();
+		}
+	}
+}
+#endif
---
+++ src/Controls/tests/TestCases/Issues/Issue14793.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[Issue(IssueTracker.Github, 14793, "[Android] Toolbar's text style for default theme has too low contrast", PlatformAffected.Android)]
+	public class Issue14793NavPage : NavigationPage
+	{
+		public Issue14793NavPage() : base(new Issue14793()) { }
+
+		public class Issue14793 : ContentPage
+		{
+			public Issue14793()
+			{
+				Title = "Title";
+				Content = new VerticalStackLayout()
+				{
+					new Label()
+					{
+						Text = "Hello, World!",
+						AutomationId = "label"
+					}
+				};
+			}
+		}
+	}
+}
